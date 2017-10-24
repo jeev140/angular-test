f944b9f1ebd2285a47c3b835e945bc348a108c7e
Jeevan <jee_ro@yahoo.co.in>
2017-10-25 01:56:22 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Injectable }     from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	studentData=null;
+   constructor(private http: Http) {
+
+         this.getJSON().subscribe(data => this.studentData=data, error => console.log(error));
+       
+    }
+     public getJSON(): Observable<any> {
+         return this.http.get("assets/document.json")
+                         .map((res:any) => res.json());
+
+     }
+                   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {DataTableModule} from "angular2-datatable";
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+     DataTableModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
